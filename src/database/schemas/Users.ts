46ddd4da5566105d85450c5e5b39bc75f1f3d942
BradleyParkerDev <bradleyscottparker@outlook.com>
2025-04-18 07:11:55 -0500

    removed images schema

--- src/database/schemas/Users.ts
+++ src/database/schemas/Users.ts
@@ -2,7 +2,6 @@ import { pgTable, text, uuid, timestamp } from 'drizzle-orm/pg-core';
 
 const User = pgTable('users', {
 	userId: uuid('user_id').primaryKey().defaultRandom().unique(), // UUID v4 primary key
-	userImage: text('user_image'),
 	firstName: text('first_name').notNull(), // Not nullable text field
 	lastName: text('last_name').notNull(), // Not nullable text field
 	emailAddress: text('email_address').unique().notNull(), // Unique and not nullable
@@ -10,8 +9,6 @@ const User = pgTable('users', {
 	password: text('password').notNull(), // Password field
 	lastUpdated: timestamp('last_updated').defaultNow().notNull(), // Update timestamp on modification
 	createdAt: timestamp('created_at').defaultNow().notNull() // Only set on creation
-
-
 });
 
 export default User;
